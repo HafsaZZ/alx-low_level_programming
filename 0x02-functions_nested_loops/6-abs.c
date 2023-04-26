b611ab4404e33026ced01a4f7529bfa7f17fcdf3
HafsaZZ <zaidi.hafsa6@gmail.com>
2023-04-25 17:56:21 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value for an interger
+ * @n: interger to be tested
+ * Return: 0 or 1
+ */
+
+int _abs(int n)
+
+{
+	if(i>=0){
+		return(i);
+	}
+	else{
+		return(i*-1);
+	}
+}
