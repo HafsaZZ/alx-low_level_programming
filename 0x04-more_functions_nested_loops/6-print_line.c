731ed867997bca9b346bd00ca7c2a116b0e33df5
HafsaZZ <zaidi.hafsa6@gmail.com>
2023-04-27 17:13:26 -0700

    uppdates

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,14 +4,17 @@
  * @n : number of _ to be printed
  * Return:void
  */
+
 void print_line(int n)
 {
-int i = 0;
 
-while (i <= n)
-{
-	_putchar('_');
-	i++;
-}
-_putchar('\n');
+	int i = 0;
+
+	while (i < n)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+
 }
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,22 +4,26 @@
  * @n : number of \\ to be printed
  * Return:void
  */
+
 void print_diagonal(int n)
 {
-int i = 0, j;
 
-while (i < n )
-{
-	j = 0;
-	while (j < i)
+	int i = 0, j;
+
+	while (i < n && n > 0)
 	{
-		_putchar(' ');
-		j++;
+		j = 0;
+		while (j < i)
+		{
+			_putchar(' ');
+			j++;
+		}
+
+		_putchar('\\');
+		_putchar('\n');
+		i++;
 	}
-	_putchar('\\');
-	_putchar('\n');
-	i++;
-}
-if (i == 0)
-	_putchar('\n');
+	if (i == 0)
+		_putchar('\n');
+
 }
