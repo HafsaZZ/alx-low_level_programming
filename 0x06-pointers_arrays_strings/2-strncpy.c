d8976a81fdf8227be273ff810b6fac2974cc6802
HafsaZZ <zaidi.hafsa6@gmail.com>
2023-05-04 14:33:23 -0700

    pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *_strncpy - copies the string pointed to by src.
+ *
+ * @dest: pointer to the buffer.
+ * @src: pointer to the source string.
+ * @n: length of src.
+ * Return: char
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+/*a function that concatenates two strings*/
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+/*function that copies a string.*/
+char *_strncpy(char *dest, char *src, int n);
+/*a function that compares two strings.*/
+int _strcmp(char *s1, char *s2);
+/*a function that reverses the content of an array of integers*/
+void reverse_array(int *a, int n);
+/*a function that changes all lowercase letters of a string to uppercase*/
+char *string_toupper(char *);
+/*a function that capitalizes all words of a string*/
+char *cap_string(char *);
+/* a function that encodes a string into 1337*/
+char *leet(char *);
+int _putchar(char c);
