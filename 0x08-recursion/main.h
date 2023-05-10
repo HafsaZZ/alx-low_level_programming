e4bda734d6907606c1c5ab287079d88baf044e3e
HafsaZZ <zaidi.hafsa6@gmail.com>
2023-05-10 16:40:36 -0700

    moore updaaaaaaaates

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,15 @@
+int _putchar(char c);
+/*a function that prints a string, followed by a new line.*/
+void _puts_recursion(char *s);
+/*a function that prints a string in reverse*/
+void _print_rev_recursion(char *s);
+/*a function that returns the length of a string*/
+int _strlen_recursion(char *s);
+/*a function that returns the factorial of a given number*/
+int factorial(int n);
+/*a function that returns the value of x raised to the power of y*/
+int _pow_recursion(int x, int y);
+/*a function that returns the natural square root of a number*/
+int _sqrt_recursion(int n);
+/*a function that returns 1 if the input integer is a prime number, otherwise return 0*/
+int is_prime_number(int n);
