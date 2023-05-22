74cb2d57b45101be14999890370c99d909d2427a
HafsaZZ <zaidi.hafsa6@gmail.com>
2023-05-22 13:06:25 -0700

    free dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,9 +1,9 @@
 #include "dog.h"
 #include <stdlib.h>
- /**
-  * free_dog - free dogs data memory
-  * @d: a dog structure
-  */
+/**
+ * free_dog - free dogs data memory
+ * @d: dog structure
+ */
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
