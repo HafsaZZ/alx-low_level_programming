aafe574b1f0b354a278293718bd7dad623efbb4f
HafsaZZ <zaidi.hafsa6@gmail.com>
2023-05-24 07:50:12 -0700

    task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,7 +3,7 @@
 #include <stddef.h>
 /*a function that prints a name*/
 void print_name(char *name, void (*f)(char *));
-/*a function that executes a function given as a parameter on each element of an array*/
+/*a function that executes a function given as a parameter*/
 void array_iterator(int *array, size_t size, void (*action)(int));
 /*a function that searches for an integer*/
 int int_index(int *array, int size, int (*cmp)(int));
