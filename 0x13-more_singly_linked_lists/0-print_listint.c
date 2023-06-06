59e96084c4f85084720e5aee39d5486c71ddcc34
HafsaZZ <zaidi.hafsa6@gmail.com>
2023-06-06 06:53:45 -0700

    upppdaaate

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,9 +10,12 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t num_nodes = 0;
-	const listint_t *current = h;
 
-	for (num_nodes = 0; current != NULL; num_nodes++, current = current->next)
-		printf("%d\n", current->n);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		num_nodes++;
+	}
 	return (num_nodes);
 }
