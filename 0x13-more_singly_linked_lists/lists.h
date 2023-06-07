9f89915c90d1c07e0d52a2a5d00c80cf77b1f9bd
HafsaZZ <zaidi.hafsa6@gmail.com>
2023-06-06 18:12:36 -0700

    UUupdate

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -49,5 +49,6 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 /*finds the loop in a linked list*/
 listint_t *find_listint_loop(listint_t *head);
+int _putchar(char c);
 
 #endif
